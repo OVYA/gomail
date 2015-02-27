d2429f567789e840bf903ef2ffb63224559e30bf
pivaldi <contact@piprime.fr>
2015-02-27 18:00:12 +0100

    Fix https://github.com/go-gomail/gomail/issues/21

--- gomail.go
+++ gomail.go
@@ -151,9 +151,9 @@ func (msg *Message) SetDateHeader(field string, date time.Time) {
 	msg.header[field] = []string{msg.FormatDate(date)}
 }
 
-// FormatDate formats a date as a valid RFC 5322 date.
+// FormatDate formats a date as a valid RFC RFC822 date.
 func (msg *Message) FormatDate(date time.Time) string {
-	return date.Format(time.RFC822Z)
+	return date.Format(time.RFC822)
 }
 
 // GetHeader gets a header field.
